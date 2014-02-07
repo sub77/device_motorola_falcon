edeb8921e3d6cf316bb38f84d1baed61257f8aaf
Jon Kagan <darkspadez@gmail.com>
2014-02-06 22:46:23 -0500

    Slimify

--- slim.mk
+++ slim.mk
@@ -1,5 +1,5 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Slim
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 720
@@ -7,6 +7,6 @@ TARGET_SCREEN_HEIGHT := 1280
 
 # Release name
 PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := cm_falcon
+PRODUCT_NAME := slim_falcon
 
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
