bdc905d250a485fcc3e0e2244f3a9e9ade3e1bae
Josue Rivera <prbassplayer@gmail.com>
2014-07-31 16:06:20 +0200

    Falcon: Quick fix for strange inherit order
    Change-Id: Ib8e50e8fa53e49f92ac6a4720ca7512c2bcba0ac
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- slim.mk
+++ slim.mk
@@ -5,9 +5,9 @@ $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 TARGET_SCREEN_WIDTH := 720
 TARGET_SCREEN_HEIGHT := 1280
 
+$(call inherit-product, device/motorola/falcon/full_falcon.mk)
+
 # Release name
 PRODUCT_RELEASE_NAME := MOTO G
 PRODUCT_NAME := slim_falcon
 PRODUCT_DEVICE := falcon
-
-$(call inherit-product, device/motorola/falcon/full_falcon.mk)
