7cab1c14150f5d622fd68c9f976c4748f4604824
Jon Kagan <darkspadez@users.noreply.github.com>
2015-02-06 14:57:13 -0500

    Update and rename cm.mk to slim.mk

---
+++ slim.mk
@@ -0,0 +1,5 @@
+$(call inherit-product, device/motorola/falcon/full_falcon.mk)
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+PRODUCT_RELEASE_NAME := MOTO G
+PRODUCT_NAME := slim_falcon
